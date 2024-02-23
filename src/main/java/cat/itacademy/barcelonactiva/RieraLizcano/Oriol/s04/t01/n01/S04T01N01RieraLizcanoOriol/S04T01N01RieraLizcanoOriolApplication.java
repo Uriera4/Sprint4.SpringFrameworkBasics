dc9ea0641b4dda158a9facbae23e20e83dcfd8d8
urier <orarpione@gmail.com>
2024-02-23 15:43:52 +0100

    "nivell1"

--- src/main/java/cat/itacademy/barcelonactiva/RieraLizcano/Oriol/s04/t01/n01/S04T01N01RieraLizcanoOriol/S04T01N01RieraLizcanoOriolApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/RieraLizcano/Oriol/s04/t01/n01/S04T01N01RieraLizcanoOriol/S04T01N01RieraLizcanoOriolApplication.java
@@ -2,7 +2,6 @@ package cat.itacademy.barcelonactiva.RieraLizcano.Oriol.s04.t01.n01.S04T01N01Rie
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class S04T01N01RieraLizcanoOriolApplication {
