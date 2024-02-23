dbd488b11afef3a0ed0938f2f45310833beeccaf
urier <orarpione@gmail.com>
2024-02-23 15:43:00 +0100

    "nivell1"

---
+++ src/main/java/cat/itacademy/barcelonactiva/RieraLizcano/Oriol/s04/t01/n01/S04T01N01RieraLizcanoOriol/S04T01N01RieraLizcanoOriolApplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.barcelonactiva.RieraLizcano.Oriol.s04.t01.n01.S04T01N01RieraLizcanoOriol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class S04T01N01RieraLizcanoOriolApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01RieraLizcanoOriolApplication.class, args);
+	}
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/RieraLizcano/Oriol/s04/t01/n01/S04T01N01RieraLizcanoOriol/S04T01N01RieraLizcanoOriolApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.RieraLizcano.Oriol.s04.t01.n01.S04T01N01RieraLizcanoOriol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01RieraLizcanoOriolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
